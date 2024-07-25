0b1fd5b6388ca2b8c06e7af3273b3e3ec04e8a32
Sambhav-Jha <b23mt1036@iitj.ac.in>
2024-07-25 17:07:02 +0530

    some tweaks + footer

---
+++ festpage_project/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../Styles/main.css";
+
+function Footer() {
+
+    return(
+        <footer className="foot">
+            <p>&copy; {new Date().getFullYear()} Sambhav Jha</p>
+        </footer>
+    );
+
+}
+
+export default Footer
